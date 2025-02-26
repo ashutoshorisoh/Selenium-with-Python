cf8fd6abd38d5715b98d7dcafdd7a6b45fd52655
Ashutosh <ashutoshorisoh@gmail.com>
2025-02-26 14:55:14 +0530

    storing data from excel into dict

---
+++ storingExcelData.py
@@ -0,0 +1,28 @@
+import openpyxl
+book=openpyxl.load_workbook(r'C:\Users\ASHUTOSH\Documents\python excel.xlsx')
+sheet=book.active  # this automatically takes the first active sheet
+# or sheer=book["sheet1] #this way we can define sheet name explicitly
+#print(sheet.title) #output = "Sheet1"
+
+#print(sheet.max_row)
+#print(sheet.max_column)
+
+#cell=sheet.cell(row=1, column=2).value="ashutosh" #this won't  change the sheet value, just change here in the terminal, but will hold changed value in entire program
+#print(cell)
+
+check=sheet['a2'].value
+#print(check) #another simple way of getting value of a particular cell
+
+#print(cell)
+maxRow= sheet.max_row
+maxColumn=sheet.max_column
+
+Lict={}
+
+for r in range(1, maxRow+1):
+    Lict[sheet.cell(row=r, column=1).value]=[]
+
+for i in range(1, maxRow+1):
+        for j in range(2, maxColumn + 1):
+            Lict[sheet.cell(row=i, column=1).value].append(sheet.cell(row=i, column=j).value)
+print(Lict)
